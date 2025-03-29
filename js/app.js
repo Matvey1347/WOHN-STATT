d880eb40e6f5437dec380ad76521160900443759
Matvey1347 <09102005w@gmail.com>
2025-03-29 01:47:55 +0100

    Init

---
+++ js/app.js
@@ -0,0 +1,17 @@
+import MainClass from "./classes/main-classes.js";
+import { initHeader } from "./header/index.js";
+import "./animation.js";
+
+const app = new MainClass();
+
+initHeader(app);
+
+// Swipers
+new Swiper(".portfolio__swiper", {
+  spaceBetween: 20,
+  slidesPerView: "auto",
+  navigation: {
+    nextEl: ".portfolio__swiper_next",
+    prevEl: ".portfolio__swiper_prev",
+  },
+});
